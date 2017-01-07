8448ed08cf17eaa6b7482e934240129a6aaad39f
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-07 12:32:51 +0000

    Changed server port

--- src/app/helpers/url-endpoints.ts
+++ src/app/helpers/url-endpoints.ts
@@ -3,6 +3,6 @@ export class UrlEndpoints {
   constructor() {
   }
 
-  public static BASE_URL :string ="http://127.0.0.1:8080";
+  public static BASE_URL :string ="http://127.0.0.1:8085";
   public static TUTOR_ENDPOINT :string =UrlEndpoints.BASE_URL+"/tutor";
 }
