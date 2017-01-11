9f9d9d3aa79adccafac8675dd4228618938a644d
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-11 16:49:18 +0000

    UrlEnpoint updated to production

--- src/app/helpers/url-endpoints.ts
+++ src/app/helpers/url-endpoints.ts
@@ -2,7 +2,7 @@ export class UrlEndpoints {
 
   constructor() {
   }
-
-  public static BASE_URL :string ="http://127.0.0.1:8085";
+  public static BASE_URL :string ="http://timetable-api.njs.jelastic.vps-host.net/timetable-technical";
+  //public static BASE_URL :string ="http://127.0.0.1:8085";
   public static TUTOR_ENDPOINT :string =UrlEndpoints.BASE_URL+"/tutor";
 }
