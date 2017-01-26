7e1a21ae9988256f2e99927fe1d4e974177d8cdd
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-26 13:42:33 +0000

    Added implementation for DepartmentArrayResponsepayload and single entities

--- src/app/models/department-array-repsponse-payload.ts
+++ src/app/models/department-array-repsponse-payload.ts
@@ -1,2 +1,8 @@
-export class DepartmentArrayRepsponsePayload {
+import {GeneralResponsePayload} from "./general-response-payload";
+import {DepartmentEntity} from "./department-entity";
+export class DepartmentArrayRepsponsePayload extends GeneralResponsePayload {
+
+  constructor(status: number, message: string, responseObject: Array<DepartmentEntity>) {
+    super(status, message, responseObject);
+  }
 }
--- src/app/models/department-response-payload.ts
+++ src/app/models/department-response-payload.ts
@@ -1,2 +1,8 @@
-export class DepartmentResponsePayload {
+import {GeneralResponsePayload} from "./general-response-payload";
+import {DepartmentEntity} from "./department-entity";
+export class DepartmentResponsePayload extends GeneralResponsePayload{
+
+  constructor(status: number, message: string, responseObject:DepartmentEntity) {
+    super(status, message, responseObject);
+  }
 }
