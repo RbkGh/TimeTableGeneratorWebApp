57deb06b4ad1a026f685695fb951e30b56e37c6a
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-28 00:33:23 +0000

    Department Management Module complete,not fully tested.

--- src/app/components/main-content/department/department.component.ts
+++ src/app/components/main-content/department/department.component.ts
@@ -8,137 +8,264 @@ import {Tutor} from "../../../models/TutorResponsePayload";
 import {TutorsArrayResponsePayload} from "../../../models/tutors-array-response-payload";
 import {DepartmentResponsePayload} from "../../../models/department-response-payload";
 
-declare var swal:any;
+declare var swal: any;
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
   styleUrls: ['./department.component.css'],
-  providers : [DepartmentService,TutorService]
+  providers: [DepartmentService, TutorService]
 })
 export class DepartmentComponent implements OnInit {
 
-  departments:Array<DepartmentEntity>;
-  noOfDepartments:number;
-  isDepartmentsListEmpty:boolean=false;
+  departments: Array<DepartmentEntity>;
+  noOfDepartments: number;
+  isDepartmentsListEmpty: boolean = false;
 
-  formIsValid:boolean;
-  isTutorsListEmpty:boolean=false;
-  addDeptForm:FormGroup;
+  formIsValid: boolean;
+  isTutorsListEmpty: boolean = false;
+  addDeptForm: FormGroup;
+  updateDeptForm: FormGroup;
+  updateDeptFormHODvalue:any;
 
-  tutors:Array<Tutor>;
-  tutorNames:Array<any>;
+  tutors: Array<Tutor>;
+  tutorNames: Array<any>;
 
   @ViewChild('modalAddDept')
   modalAddDept: ModalComponent;
-  deptHODtutorId:string;
+  @ViewChild('modalUpdateDept')
+  modalUpdateDept: ModalComponent;
+  deptHODtutorId: string;
 
-  constructor(public departmentService:DepartmentService,
-              public tutorService:TutorService,
-              public formBuilder:FormBuilder) { }
+  constructor(public departmentService: DepartmentService,
+              public tutorService: TutorService,
+              public formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
     this.getAllDepartments();
     this.buildAddDeptForm();
+    this.buildUpdateDeptForm();
   }
 
-  getAllDepartments():void{
+  getAllDepartments(): void {
     this.departmentService.getAllDepartments().subscribe(
-      r=>{
-        if(r.status===0) {
+      r => {
+        if (r.status === 0) {
           this.departments = r.responseObject;
           this.noOfDepartments = r.responseObject.length;
           if (r.responseObject.length === 0) {
             this.isDepartmentsListEmpty = true;
-          }else{
+          } else {
             this.isDepartmentsListEmpty = false;
           }
-        }else{
-          swal("Error",r.message,"error");
+        } else {
+          swal("Error", r.message, "error");
         }
       },
-      e=>{
-        swal("Error","Ensure you have a working internet connection,other wise try again later.","error");
+      e => {
+        swal("Error", "Ensure you have a working internet connection,other wise try again later.", "error");
       }
     )
   }
 
-  getAllTutorsToAddToDept():void{
+  getAllTutorsToAddToDept(): void {
     this.tutorService.getAllTutors()
       .subscribe(
-        (response:TutorsArrayResponsePayload)=>{
-          if(response.status === 0) {
-            if(response.responseObject.length ===0) {
+        (response: TutorsArrayResponsePayload) => {
+          if (response.status === 0) {
+            if (response.responseObject.length === 0) {
               this.modalAddDept.dismiss();
               this.isTutorsListEmpty = true;
-              swal("No Tutors Created","Kindly add the H.O.D to the tutors first.","error");
-            }else{
-              console.log("Tutors to add to department=",response.responseObject);
+              swal("No Tutors Created", "Kindly add the H.O.D to the tutors first.", "error");
+            } else {
+              console.log("Tutors to add to department=", response.responseObject);
               this.tutors = response.responseObject;
               this.tutorNames = this.getTutorNames(response.responseObject);
             }
-          }else{
-            swal("Error",response.message,"error");
+          } else {
+            swal("Error", response.message, "error");
           }
         },
-        error=>{
-          swal("Error","Something went wrong","error");
+        error => {
+          swal("Error", "Something went wrong", "error");
         }
       )
   }
 
-  getTutorNames(tutors:Array<Tutor>):Array<any>{
-    let tutorNamesStrings:Array<any>=[];
-    for(let i:number =0; i< tutors.length;i++ ){
-      tutorNamesStrings[i]={
-        id:tutors[i].id,
-        text:tutors[i].firstName+' '+tutors[i].surName
+  getTutorNames(tutors: Array<Tutor>): Array<any> {
+    let tutorNamesStrings: Array<any> = [];
+    for (let i: number = 0; i < tutors.length; i++) {
+      tutorNamesStrings[i] = {
+        id: tutors[i].id,
+        text: tutors[i].firstName + ' ' + tutors[i].surName
       };
     }
     return tutorNamesStrings;
   }
 
-  addDepartment(addDeptForm:FormGroup):void{
+  addDepartment(addDeptForm: FormGroup): void {
     let deptHODtutorId = this.deptHODtutorId;
-    console.log('deptHODtutorId :',deptHODtutorId);
-    let departmentEntity:DepartmentEntity =new DepartmentEntity(
-      null,addDeptForm.value.deptName,deptHODtutorId,'');
+    console.log('deptHODtutorId :', deptHODtutorId);
+    let departmentEntity: DepartmentEntity = new DepartmentEntity(
+      null, addDeptForm.value.deptName, deptHODtutorId, '');
     // departmentEntity.deptHODtutorId = this.deptHODtutorId;
     // departmentEntity.deptName = addDeptForm.value.deptName;
 
     this.departmentService.createDepartment(departmentEntity).subscribe(
-      (r:DepartmentResponsePayload)=>{
-        if(r.status===0) {
+      (r: DepartmentResponsePayload) => {
+        if (r.status === 0) {
           this.modalAddDept.dismiss();
           this.ngOnInit();
-          swal("Success","Department Added Successfully","success");
-        }else{
-          swal("Error",r.message,"error");
+          swal("Success", "Department Added Successfully", "success");
+        } else {
+          swal("Error", r.message, "error");
         }
       },
-      (error:any)=>{
-        swal("Error","Something went wrong,please Try Again","error");
+      (error: any) => {
+        swal("Error", "Something went wrong,please Try Again", "error");
       }
     );
   }
 
-  public selectedHOD(value:any):void {
+  updateDepartment(addDeptForm: FormGroup): void {
+    let deptHODtutorId = this.deptHODtutorId;
+    console.log('deptHODtutorId :', deptHODtutorId);
+    let departmentEntity: DepartmentEntity = new DepartmentEntity(
+      null, addDeptForm.value.deptName, deptHODtutorId, '');
+
+    this.departmentService.updateDepartment(departmentEntity).subscribe(
+      (r: DepartmentResponsePayload) => {
+        if (r.status === 0) {
+          this.modalAddDept.dismiss();
+          this.ngOnInit();
+          swal("Success", "Department Updated Successfully", "success");
+        } else {
+          swal("Error", r.message, "error");
+        }
+      },
+      (error: any) => {
+        swal("Error", "Something went wrong,please Try Again", "error");
+      }
+    );
+  }
+
+  deleteDepartment(currentDeptId: string): void {
+    console.log("currentDeptId=" + currentDeptId);
+    swal({
+        title: "Are you sure?",
+        text: "This will delete Department Permanently!!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel please!",
+        closeOnConfirm: false,
+        closeOnCancel: false,
+        showLoaderOnConfirm: true
+      },
+      (isConfirm) => {
+        if (isConfirm) {
+          /**
+           * always use arrow functions otherwise this collides with typescript's this,hence leading to undefined.
+           */
+          this.departmentService.deleteDepartment(currentDeptId)
+            .subscribe(
+              (r)=>{
+                if(r.status===0){
+                  this.ngOnInit();
+                  swal("Successful","Department deleted successfully","success");
+                }else{
+                  swal("Error",r.message,"error");
+                }
+              },
+              error=>{
+                swal("Error","Something went wrong.Try again","error");
+              }
+            );
+
+        } else {
+          swal("Cancelled", "Department was not deleted", "error");
+        }
+      });
+
+
+  }
+
+  deleteAllDepartments(): void {
+
+    swal({
+        title: "Are you sure?",
+        text: "This will delete all departments Permanently!!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel please!",
+        closeOnConfirm: false,
+        closeOnCancel: false,
+        showLoaderOnConfirm: true
+      },
+      (isConfirm) => {
+        if (isConfirm) {
+          /**
+           * always use arrow functions otherwise this collides with typescript's this,hence leading to undefined.
+           */
+          this.departmentService.deleteAllDepartments()
+            .subscribe(
+              (r)=>{
+                if(r.status===0){
+                  this.ngOnInit();
+                  swal("Successful","All Departments deleted successfully","success");
+                }else{
+                  swal("Error",r.message,"error");
+                }
+              },
+              error=>{
+                swal("Error","Something went wrong.Try again","error");
+              }
+            );
+
+        } else {
+          swal("Cancelled", "Departments were not deleted", "error");
+        }
+      });
+
+
+  }
+
+  public selectedHOD(value: any): void {
     console.log('Selected value is: ', value);
-    console.log('value id=',value.id);
+    console.log('value id=', value.id);
     this.deptHODtutorId = value.id;
   }
-  public refreshValue(value:any):void {
+
+  public refreshValue(value: any): void {
     //this.value = value;
-    console.log('Data =',value);
+    console.log('Data =', value);
   }
-  public typedHODName(value:any):void {
+
+  public typedHODName(value: any): void {
     console.log('New search input: ', value);
   }
 
-  buildAddDeptForm():void{
+  buildAddDeptForm(): void {
     this.addDeptForm = this.formBuilder.group({});
-    this.addDeptForm.addControl('deptName',new FormControl('',Validators.required));
+    this.addDeptForm.addControl('deptName', new FormControl('', Validators.required));
 
     this.addDeptForm.valueChanges
+      .subscribe(data => this.onAddDeptFormValueChanged(data));
+    this.onAddDeptFormValueChanged(); // (re)set validation messages now
+  }
+
+  buildUpdateDeptForm(deptEntity?: DepartmentEntity): void {
+    if (typeof deptEntity === "undefined") {
+      this.updateDeptForm = this.formBuilder.group({});
+      this.updateDeptForm.addControl('deptNameUpdate', new FormControl('', Validators.required));
+    } else {
+      this.updateDeptForm = this.formBuilder.group({});
+      this.updateDeptForm.addControl('deptNameUpdate', new FormControl(deptEntity.deptName, Validators.required));
+    }
+    this.updateDeptForm.valueChanges
       .subscribe(data => this.onAddDeptFormValueChanged(data));
     this.onAddDeptFormValueChanged(); // (re)set validation messages now
   }
@@ -171,17 +298,31 @@ export class DepartmentComponent implements OnInit {
 
 
   formErrors = {
-    'deptName': ''
+    'deptName': '',
+    'deptNameUpdate': ''
   };
   validationMessages = {
     'deptName': {
       'required': 'Department Name is required.'
+    },
+    'deptNameUpdate': {
+      'required': 'Department Name is required.'
     }
   };
 
-  openAddDepartmentModal():void{
+  openAddDepartmentModal(): void {
     this.modalAddDept.open();
     this.getAllTutorsToAddToDept();
+  }
+
+  openEditDepartmentModal(deptEntity: DepartmentEntity): void {
+    this.modalUpdateDept.open();
+    this.buildUpdateDeptForm(deptEntity);
+    this.getAllTutorsToAddToDept();
+  }
+
+  refreshPage(): void {
+    this.ngOnInit();
   }
 
 }
