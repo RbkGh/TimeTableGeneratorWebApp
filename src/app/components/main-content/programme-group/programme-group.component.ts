48cbcea8e6fdb542a013a719a0f14a60ba210faf
Ace P Rbk <rbk.unlimited@gmail.com>
2017-02-02 18:21:40 +0000

    Added programmeGroup Addition modal with all validations,next is implementation.

--- src/app/components/main-content/programme-group/programme-group.component.ts
+++ src/app/components/main-content/programme-group/programme-group.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ProgrammeGroupService} from "../../../services/programme-group.service";
 import {ProgrammeGroupEntity} from "../../../models/programme-group-entity";
+import {ModalComponent} from "ng2-bs3-modal/components/modal";
+import {FormBuilder, FormGroup, FormControl, Validators} from "@angular/forms";
 
 declare var swal: any;
 @Component({
@@ -12,14 +14,22 @@ declare var swal: any;
 export class ProgrammeGroupComponent implements OnInit {
 
   noOfProgrammeGroups: number;
+  formIsValid: boolean;
   isProgrammeGroupListEmpty: boolean = false;
   programmeGroups: Array<ProgrammeGroupEntity>;
+  yearGroupList:Array<number>;
 
-  constructor(private programmeGroupService: ProgrammeGroupService) {
+  @ViewChild('modalAddProgrammeGroup')
+  modalAddProgrammeGroup: ModalComponent;
+
+  addProgrammeGroupForm: FormGroup;
+
+  constructor(private programmeGroupService: ProgrammeGroupService,private formBuilder:FormBuilder) {
   }
 
   ngOnInit() {
     this.getAllProgrammeGroups();
+    this.buildAddDepartmentForm();
   }
 
   refreshPage(): void {
@@ -27,6 +37,7 @@ export class ProgrammeGroupComponent implements OnInit {
   }
 
   openAddProgrammeGroupModal(): void {
+    this.modalAddProgrammeGroup.open();
 
   }
 
@@ -70,10 +81,6 @@ export class ProgrammeGroupComponent implements OnInit {
       });
   }
 
-  deleteAllProgrammeGroups(): void {
-
-  }
-
   getAllProgrammeGroups(): void {
     this.programmeGroupService.getAllProgrammeGroups()
       .subscribe(
@@ -95,5 +102,108 @@ export class ProgrammeGroupComponent implements OnInit {
       );
   }
 
+  buildAddDepartmentForm():void{
+    this.addProgrammeGroupForm = this.formBuilder.group({});
+    this.addProgrammeGroupForm.addControl('programmeFullName', new FormControl('', Validators.required));
+    this.addProgrammeGroupForm.addControl('programmeInitials', new FormControl('', Validators.required));
+    this.addProgrammeGroupForm.addControl('yearGroup', new FormControl('', Validators.required));
+    this.addProgrammeGroupForm.addControl('technicalWorkshopOrLabRequired', new FormControl('', Validators.required));
+
+    this.addProgrammeGroupForm.valueChanges.subscribe(
+      data=>{this.onAddProgrammeGroupValueChanged(data)}
+    );
+
+    this.onAddProgrammeGroupValueChanged(); //reset validation messages
+  }
+
+  addProgrammeGroup(addProgrammeGroupForm:FormGroup):void{
+
+  }
+
+  public selectedHOD(value: any): void {
+    console.log('Selected value is: ', value);
+    console.log('value id=', value.id);
+  }
+
+
+
+  public refreshValueMultiple(value: any): void {
+    let yearGroupList:Array<number> =[];
+
+    for(let i:number=0;i<value.length;i++) {
+      yearGroupList.push(value[i].id);
+    }
+    this.yearGroupList = yearGroupList;
+    console.log('Data =', value);
+    console.log('YearGroupList =', this.yearGroupList);
+  }
+
+  /**
+   * ng-select library takes data in the form of {id,text} objects.
+   *
+   * @returns {Array<any>}
+   */
+  getYearGroupItems(): Array<any> {
+    let yearGroupItems: Array<any> = [];
+    for (let i: number = 1; i <= 3; i++) {
+      yearGroupItems[i] = {
+        id: i,
+        text: 'FORM '+i
+      };
+    }
+    console.info('YearGroup Objects to be populated in dropdown: ',yearGroupItems);
+    return yearGroupItems;
+  }
+
+  onAddProgrammeGroupValueChanged(data?: any): void {
+    if (!this.addProgrammeGroupForm) {
+      return;
+    }
+    const form = this.addProgrammeGroupForm;
+
+    for (const field in this.formErrors) {
+      // clear previous error message and styling (if any)
+      this.formErrors[field] = '';
+      this.formIsValid = false;
+
+      const control = form.get(field);
+      //if form is touched,dirty, and if the control is invalid,per validation rule,
+      if (control && control.dirty && !control.valid) {
+
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+          this.formIsValid = false;
+        }
+      } else {
+        this.formIsValid = true;
+      }
+    }
+  }
+
+  formErrors = {
+    'programmeFullName': '',
+    'programmeInitials': '',
+    'yearGroup': '',
+    'yearGroupList': '',
+    'technicalWorkshopOrLabRequired': ''
+  };
+  validationMessages = {
+    'programmeFullName': {
+      'required': 'Programme Full Name is required.'
+    },
+    'programmeInitials': {
+      'required': 'Department Name is required.'
+    },
+    'yearGroup': {
+      'required': 'YearGroup is required.'
+    },
+    'yearGroupList': {
+      'required': 'Field is required'
+    },
+    'technicalWorkshopOrLabRequired': {
+      'required': 'Field is required.'
+    }
+  };
 
 }
