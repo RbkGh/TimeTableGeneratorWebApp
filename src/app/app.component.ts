4553972c236f3300af86086de3755061c0b6ae76
Ace P Rbk <rbk.unlimited@gmail.com>
2017-02-12 21:41:36 +0000

    ADDED NEW TODO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import {LoginComponent} from "./components/login/login.component";
 import {HomeComponent} from "./components/home/home.component";
 import {Router} from "@angular/router";
 
-
+/**
+ * TODO DISABLE REASSIGNING DEPARTMENT HEAD TO DIFFERENT DEPT HEAD ,CURRENTLY SETTING THIS REMOVES
+ * DEPARTMENT HEAD AND THERE IS NO OPTION TO RESTORE IT.
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
