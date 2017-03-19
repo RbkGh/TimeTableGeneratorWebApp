c6ac70127f48339926a05cc4ad83525e60a97aa5
Ace P Rbk <rbk.unlimited@gmail.com>
2017-03-19 01:47:13 +0000

    Added extra methods in timetable generation component.

--- src/app/components/main-content/time-table/time-table.component.ts
+++ src/app/components/main-content/time-table/time-table.component.ts
@@ -20,6 +20,9 @@ export class TimeTableComponent implements OnInit {
   generateTimeTableForm: FormGroup;
   formIsValid: boolean = false;
   accessingService: boolean = false;
+  isGenerateButtonWandVisible: boolean = true;
+
+  successfullyGeneratedTimeTableMainEntity: TimeTableMainEntity;
 
   constructor(private timeTableGenerationService: TimeTableGenerationService,
               private formBuilder: FormBuilder) {
@@ -41,25 +44,73 @@ export class TimeTableComponent implements OnInit {
     let timeTableGenerationRequest: TimeTableGenerationRequest =
       new TimeTableGenerationRequest(timeTableName, timeTableYear);
 
-    console.log("TimeTable Generation Request ==>",timeTableGenerationRequest);
-    this.timeTableGenerationService.generateTimeTable(timeTableGenerationRequest).subscribe(
-      (response: TimeTableMainEntityResponsePayload) => {
-        this.accessingService = false;
-        console.info("response status = " + response.status);
-        if (response.status === 0) {
-          this.modalGenerateTimeTableDialog.dismiss();
-          this.ngOnInit();
-          swal("Success", "Subject Added Successfully", "success");
-        } else {
-          swal("Error", response.message||"Error", "error");
-        }
-      },
-      (error: any) => {
-        this.accessingService = false;
-        swal("Error", "Something went wrong,Try Again", "error");
-        console.log(error);
-      }
-    );
+    console.log("TimeTable Generation Request ==>", timeTableGenerationRequest);
+    this.isGenerateButtonWandVisible = false;
+    // this.timeTableGenerationService.generateTimeTable(timeTableGenerationRequest).subscribe(
+    //   (response: TimeTableMainEntityResponsePayload) => {
+    //     this.accessingService = false;
+    //     console.info("response status = " + response.status);
+    //     if (response.status === 0) {
+    //       this.isGenerateButtonWandVisible = false;//once the response status is 0,we can hide the generate button.
+    //       response.responseObject = this.successfullyGeneratedTimeTableMainEntity;
+    //       this.modalGenerateTimeTableDialog.dismiss();
+    //       swal("Success", "Subject Added Successfully", "success");
+    //     } else {
+    //       swal("Error", response.message || "Error", "error");
+    //     }
+    //   },
+    //   (error: any) => {
+    //     this.accessingService = false;
+    //     swal("Error", "Something went wrong,Try Again", "error");
+    //     console.log(error);
+    //   }
+    // );
+  }
+
+  /**
+   * ID of programmeGroup Type of timetable used in ng-select library
+   * @type {number}
+   */
+  TIMETABLE_TYPE_PROGRAMMEGROUP : number = 0;
+  TIMETABLE_TYPE_TUTOR : number = 1;
+  /**
+   * ng-select library takes data in the form of {id,text} objects.
+   *
+   * @returns {Array<any>}
+   */
+  public getTimeTableTypesItems(): Array<any> {
+    let timeTableTypes: Array<any> = [];
+
+    let timeTableType1: any = {
+      id: this.TIMETABLE_TYPE_PROGRAMMEGROUP,
+      text: 'Classes/ProgrammeGroups TimeTable'
+    };
+    let timeTableType2: any = {
+      id: this.TIMETABLE_TYPE_TUTOR,
+      text: 'Tutors TimeTable'
+    };
+    timeTableTypes.push([timeTableType1, timeTableType2]);
+
+    console.info('TimeTable Type Objects to be populated in dropdown: ', timeTableTypes);
+    return timeTableTypes;
+  }
+
+  public selectedTimeTableType(value: any): void {
+    console.log('Selected value is: ', value);
+    console.log('value id=', value.id);
+
+    switch (value.id) {
+      case this.TIMETABLE_TYPE_PROGRAMMEGROUP :
+        //DO IMPLEMENTATION TO SHOW ALL TIMETABLE FOR ALL PROGRAMMEGROUPS RETURNED FROM SERVICE
+            break;
+      case this.TIMETABLE_TYPE_TUTOR :
+        //DO IMPLEMENTATION TO SHOW ALL TIMETABLE FOR ALL TUTORS RETURNED FROM SERVICE.
+            break;
+      default :
+        //DEFAULT IMPLEMENTAION
+            break;
+
+    }
   }
 
   buildGenerateTimeTableForm(): void {
