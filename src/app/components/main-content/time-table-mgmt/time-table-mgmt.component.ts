0af4e7324e241210b046b4d6dd52d68bbe94317d
Ace P Rbk <rbk.unlimited@gmail.com>
2017-03-20 11:55:35 +0000

    timetable management component updated with list of timetable docs.

--- src/app/components/main-content/time-table-mgmt/time-table-mgmt.component.ts
+++ src/app/components/main-content/time-table-mgmt/time-table-mgmt.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TimeTableGenerationService} from "../../../services/time-table-generation.service";
+import {TimeTableMainEntity} from "../../../models/time-table-main-entity";
+import {TimeTableMainEntityArrayResponsePayload} from "../../../models/time-table-main-entity-array-response-payload";
 
+declare var swal: any;
 @Component({
   selector: 'app-time-table-mgmt',
   templateUrl: './time-table-mgmt.component.html',
-  styleUrls: ['./time-table-mgmt.component.css']
+  styleUrls: ['./time-table-mgmt.component.css'],
+  providers: [TimeTableGenerationService]
 })
 export class TimeTableMgmtComponent implements OnInit {
 
-  constructor() { }
+  timeTableMainEntitiesList: Array<TimeTableMainEntity>;
+  noOfTimeTableMainEntities: number;
+
+  constructor(private timeTableGenerationService: TimeTableGenerationService) {
+
+  }
 
   ngOnInit() {
+  }
+
+  getAllGeneratedTimeTables(): void {
+    this.timeTableGenerationService.getAllGeneratedTimeTables().subscribe(
+      (response: TimeTableMainEntityArrayResponsePayload) => {
+        if ((response.status === 0) && (response.responseObject.length > 0)) {
+          this.timeTableMainEntitiesList = response.responseObject;
+          this.noOfTimeTableMainEntities = response.responseObject.length;
+        } else if ((response.status === 0) && (response.responseObject.length === 0)) {
+          swal("Error", "No timetables generated currently.", "error");
+        } else {
+          swal("Error", "Something went wrong.Please Try Again", "error");
+        }
+      },
+      (error: any) => {
+        swal("Error", "Something went wrong.Please Try Again", "error");
+      }
+    );
   }
 
 }
